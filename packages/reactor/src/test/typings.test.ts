d251e8577cee5e92c6a372172908b0a0e46783f4
Homa Wong <homawong@gmail.com>
2016-12-02 11:30:37 -0800

    Update to overload version

--- packages/reactor/src/test/typings.test.ts
+++ packages/reactor/src/test/typings.test.ts
@@ -1,10 +1,15 @@
 import React = require('react')
 import { reactify, install } from '../../index'
 
-interface GridProperty {}
-interface GridState {}
-interface GridInterface {}
-
-const Grid = reactify<GridInterface, GridProperty, GridState>('grid')
+class Foo {}
+interface Props {}
+interface State {}
+interface Grid {}
+interface Panel {}
+const Grid = reactify<Grid, Props, State>('grid')
+const [ Grid1, Panel ] = reactify<Grid, Props, State, Panel, Props, State>('gird', 'panel')
+const [ Grid2, Panel2 ] = reactify('grid', 'panel')
+const [ Grid3, Panel3 ] = reactify('grid', 'panel') as [ Grid & React.Component<string, boolean>, any]
+const [ A, B, C, D, E ] = reactify('a', 'b', Foo, 'd', 'e')
 
 install({ viewport: true })
