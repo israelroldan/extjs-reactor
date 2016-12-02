e5209bb11904b2ef1d85a8ffc359a6ca4c086272
Homa Wong <homawong@gmail.com>
2016-12-01 22:47:02 -0800

    Get typings ready for 0.2

--- packages/reactor/index.d.ts
+++ packages/reactor/index.d.ts
@@ -1,6 +1,21 @@
+import React = require('react')
 
-export interface ReactifyOptions {
-    viewport: boolean
+export interface InstallOptions {
+    /**
+     * Adds a stylesheet that mimics an Ext JS Viewport by setting the html, body, and react root element to height: 100%.
+     * Set this to true when using an Ext JS component at the root of your app.
+     */
+    viewport: boolean;
 }
 
-export default function reactify(options: Partial<ReactifyOptions>);
+/**
+ * Configures React.
+ */
+export function install(options: Partial<InstallOptions>): void;
+
+/**
+ * Creates a react component for a given Ext JS component.
+ * For example: const Grid = reactify('grid');
+ * @param target xtype or instance of Ext.Class.
+ */
+export function reactify<Class, Property, State>(target: string | Function): Class & React.Component<Property, State>;
