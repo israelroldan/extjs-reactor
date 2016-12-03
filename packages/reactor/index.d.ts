6ce537622309068051adb7172a1f683fc8018278
Homa Wong <homawong@gmail.com>
2016-12-02 22:31:32 -0800

    Fix return value to Constructor

--- packages/reactor/index.d.ts
+++ packages/reactor/index.d.ts
@@ -18,8 +18,8 @@ export function install(options: Partial<InstallOptions>): void;
  * For example: const Grid = reactify('grid');
  * @param target xtype or instance of Ext.Class.
  */
-export function reactify<Class, Props, State>(target: string | Function): Class & React.Component<Props, State>;
-export function reactify<Class1, Props1, State1, Class2, Props2, State2>(target1: string | Function, target2: string | Function): [Class1 & React.Component<Props1, State1>, Class2 & React.Component<Props2, State2>];
-export function reactify<Class1, Props1, State1, Class2, Props2, State2, Class3, Props3, State3>(target1: string | Function, target2: string | Function, target3: string | Function): [Class1 & React.Component<Props1, State1>, Class2 & React.Component<Props2, State2>, Class3 & React.Component<Props3, State3>];
-export function reactify<Class1, Props1, State1, Class2, Props2, State2, Class3, Props3, State3, Class4, Props4, State4>(target1: string | Function, target2: string | Function, target3: string | Function, target4: string | Function): [Class1 & React.Component<Props1, State1>, Class2 & React.Component<Props2, State2>, Class3 & React.Component<Props3, State3>, Class4 & React.Component<Props4, State4>];
-export function reactify(...targets: (string | Function)[]): React.Component<any, any>[];
+export function reactify<Class, Props, State>(target: string | Function): Class & (new() => React.Component<Props, State>);
+export function reactify<Class1, Props1, State1, Class2, Props2, State2>(target1: string | Function, target2: string | Function): [Class1 & (new () => React.Component<Props1, State1>), Class2 & (new () => React.Component<Props2, State2>)];
+export function reactify<Class1, Props1, State1, Class2, Props2, State2, Class3, Props3, State3>(target1: string | Function, target2: string | Function, target3: string | Function): [Class1 & (new () => React.Component<Props1, State1>), Class2 & (new () => React.Component<Props2, State2>), Class3 & (new () => React.Component<Props3, State3>)];
+export function reactify<Class1, Props1, State1, Class2, Props2, State2, Class3, Props3, State3, Class4, Props4, State4>(target1: string | Function, target2: string | Function, target3: string | Function, target4: string | Function): [Class1 & (new () => React.Component<Props1, State1>), Class2 & (new () => React.Component<Props2, State2>), Class3 & (new () => React.Component<Props3, State3>), Class4 & (new () => React.Component<Props4, State4>)];
+export function reactify(...targets: (string | Function)[]): (new () => React.Component<any, any>)[];
--- packages/reactor/src/test/typings.test.tsx
+++ packages/reactor/src/test/typings.test.tsx
@@ -1,4 +1,5 @@
 import React = require('react')
+import ReactDOM = require('react-dom')
 import { reactify, install } from '../../index'
 
 class Foo {}
@@ -9,7 +10,9 @@ interface Panel {}
 const Grid = reactify<Grid, Props, State>('grid')
 const [ Grid1, Panel ] = reactify<Grid, Props, State, Panel, Props, State>('gird', 'panel')
 const [ Grid2, Panel2 ] = reactify('grid', 'panel')
-const [ Grid3, Panel3 ] = reactify('grid', 'panel') as [ Grid & React.Component<string, boolean>, any]
+const [ Grid3, Panel3 ] = reactify('grid', 'panel') as [ Grid & (new () => React.Component<string, boolean>), any]
 const [ A, B, C, D, E ] = reactify('a', 'b', Foo, 'd', 'e')
 
 install({ viewport: true })
+
+ReactDOM.render(<Grid/>, document.getElementById('root'))
