5c8aaca34e625e91109b990e04236064cbcf4a53
Homa Wong <homawong@gmail.com>
2016-12-02 23:18:47 -0800

    Simplify typings.
    Props actual equal the outside type already.

--- packages/reactor/src/test/typings.test.tsx
+++ packages/reactor/src/test/typings.test.tsx
@@ -5,12 +5,10 @@ import { reactify, install } from '../../index'
 class Foo {}
 interface Props {}
 interface State {}
-interface Grid {}
-interface Panel {}
-const Grid = reactify<Grid, Props, State>('grid')
-const [ Grid1, Panel ] = reactify<Grid, Props, State, Panel, Props, State>('gird', 'panel')
+const Grid = reactify<Props, State>('grid')
+const [ Grid1, Panel ] = reactify<Props, State, Props, State>('gird', 'panel')
 const [ Grid2, Panel2 ] = reactify('grid', 'panel')
-const [ Grid3, Panel3 ] = reactify('grid', 'panel') as [ Grid & (new () => React.Component<string, boolean>), any]
+const [ Grid3, Panel3 ] = reactify('grid', 'panel') as [ new () => React.Component<string, boolean>, any]
 const [ A, B, C, D, E ] = reactify('a', 'b', Foo, 'd', 'e')
 
 install({ viewport: true })
