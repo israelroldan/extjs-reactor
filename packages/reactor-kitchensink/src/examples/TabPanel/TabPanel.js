9680878c5a15e9c6a37abe972986172cfd4d0bd8
Mark Brocato <mark.brocato@sencha.com>
2017-01-31 18:39:03 -0500

    fix production build

--- packages/reactor-kitchensink/src/examples/TabPanel/TabPanel.js
+++ packages/reactor-kitchensink/src/examples/TabPanel/TabPanel.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { TabPanel, Panel } from '@extjs/reactor/modern';
+import { TabPanel, Panel, Component } from '@extjs/reactor/modern';
 
 export default function TabPanelExample() {
     return (
         <TabPanel height={300} width={500} shadow={true}>
             <Panel title="Tab 1">
-                Tab 1 content
+                <Component html="Tab 1 content"/>
             </Panel>
             <Panel title="Tab 2">
-                Tab 2 content
+                <Component html="Tab 2 content"/>
             </Panel>
         </TabPanel>
     )
