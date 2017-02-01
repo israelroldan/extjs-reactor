c0dbe97f739910ebfc77c37be12f06049245be40
Mark Brocato <mark.brocato@sencha.com>
2017-01-31 21:43:47 -0500

    switched examples to classes

--- packages/reactor-kitchensink/src/examples/List/List.js
+++ packages/reactor-kitchensink/src/examples/List/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { List } from '@extjs/reactor/modern';
 import data from './data';
 
@@ -7,31 +7,38 @@ Ext.require([
     'Ext.MessageBox'
 ]);
 
-export default function ListExample() {
-    const store = Ext.create('Ext.data.Store', { 
-        data,
-        grouper: {
-            groupFn: function(record) {
-                return record.get('last_name')[0];
-            }
-        },
-        sorters: ['last_name', 'first_name']
-    });
+export default class ListExample extends Component {
 
-    return (
-        <List
-            indexBar={true}
-            shadow={true}
-            itemTpl="{first_name} {last_name}"
-            grouped={true}
-            pinHeaders={true}
-            store={store}
-            onItemTap={(view, index, target, record) => {
-                Ext.toast(`You selected ${record.get('first_name')} ${record.get('last_name')}.`)
-            }}
-            onItemDisclosure={(record, btn, index) => {
-                Ext.Msg.alert('Tap', 'Disclose more info for ' + record.get('firstName'), Ext.emptyFn);
-            }}
-        />
-    )
+    constructor() {
+        super();
+        this.store = Ext.create('Ext.data.Store', { 
+            data,
+            grouper: {
+                groupFn: function(record) {
+                    return record.get('last_name')[0];
+                }
+            },
+            sorters: ['last_name', 'first_name']
+        });
+    }
+
+    render() {
+        return (
+            <List
+                indexBar={true}
+                shadow={true}
+                itemTpl="{first_name} {last_name}"
+                grouped={true}
+                pinHeaders={true}
+                store={this.store}
+                onItemTap={(view, index, target, record) => {
+                    Ext.toast(`You selected ${record.get('first_name')} ${record.get('last_name')}.`)
+                }}
+                onItemDisclosure={(record, btn, index) => {
+                    Ext.Msg.alert('Tap', 'Disclose more info for ' + record.get('first_name'), Ext.emptyFn);
+                }}
+            />
+        )
+    }
+    
 }
