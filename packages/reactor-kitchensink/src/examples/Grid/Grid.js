a62a2f062d126ce1b1fa4bc2123168f4c5e832f5
AlexanderDracka <alexander.dracka@profiq.cz>
2017-02-06 14:30:49 +0100

    Updated Grid Example

--- packages/reactor-kitchensink/src/examples/Grid/Grid.js
+++ packages/reactor-kitchensink/src/examples/Grid/Grid.js
@@ -1,59 +1,269 @@
 import React, { Component } from 'react';
-import { Grid } from '@extjs/reactor/modern';
+import { Grid, ActionSheet, Toolbar, Container, Button } from '@extjs/reactor/modern';
 import data from './data';
+import model from './model';
 
-Ext.require('Ext.grid.plugin.*')
+Ext.require('Ext.grid.plugin.*');
+Ext.require('Ext.tip.ToolTip');
+Ext.require('Ext.*');
 
 export default class GridExample extends Component {
-
     constructor() {
         super();
-
-        const model = Ext.define('MyGridModel', {
-            extend: 'Ext.data.Model',
-            fields: [{
-                name: 'id'
-            }, {
-                name: 'first_name'
-            }, {
-                name: 'last_name'
-            }, {
-                name: 'email'
-            }, {
-                name: 'gender'
-            }, {
-                name: 'ip_address'
-            }, {
-                name: 'full_name',
-                calculate: ({ first_name, last_name }) => `${first_name} ${last_name}`
-            }]
-        })
-
         this.store = Ext.create('Ext.data.Store', {
-            data,
-            model
+            data: data,
+            model: model
+        }); 
+    }
+
+    exportToXlsx() {
+        this.doExport({
+            type: 'excel07',
+            title: 'Pivot grid export demo',
+            fileName: 'GridExport.xlsx'
         });
     }
 
+    exportToXml() {
+        this.doExport({
+            type:       'excel03',
+            title:      'Pivot grid export demo',
+            fileName:   'GridExport.xml'
+        });
+    }
+
+    exportToCSV() {
+        this.doExport({
+            type:       'csv',
+            title:      'Pivot grid export demo',
+            fileName:   'GridExport.csv'
+        });
+    }
+
+    exportToTSV() {
+         this.doExport({
+            type:       'tsv',
+            title:      'Pivot grid export demo',
+            fileName:   'GridExport.csv'
+        });
+    }
+
+    exportToHtml() {
+         this.doExport({
+            type:       'html',
+            title:      'Pivot grid export demo',
+            fileName:   'GridExport.html'
+        });
+    }
+
+    doExport(config) {
+        this.refs.exportMenu.hide();
+        this.refs.grid.saveDocumentAs(config);
+    }
+
     render() {
+        var onVerifyTap = (btn) => {
+            var cell = btn.getParent(),
+                record = cell.getRecord();
+            record.set('verified', true);
+            Ext.Msg.alert('Verify', 'Verify ' + record.get('forename') + ' ' + record.get('surname'));
+        }
+
+        var salarySummaryRenderer = (value) => {
+            return Ext.util.Format.usMoney(value);
+        }
+    
         return (
-            <Grid
-                title="Grid" 
-                store={this.store}
-                shadow={true} 
-                plugins={[
-                    { type: 'columnresizing'}
-                ]}
-                columns={[
-                    { text: 'ID', dataIndex: 'id', width: 80, },
-                    { text: 'First Name', dataIndex: 'first_name', width: 125 },
-                    { text: 'Last Name', dataIndex: 'last_name', width: 125 },
-                    { text: 'Full Name', dataIndex: 'full_name', width: 150 },
-                    { text: 'Email', dataIndex: 'email', width: 300 },
-                    { text: 'Gender', dataIndex: 'gender', width: 100 },
-                    { text: 'IP Address', dataIndex: 'ip_address', width: 200 },
-                ]}
-            />
+            <Container layout="fit">
+                <ActionSheet ref="exportMenu">
+                    <Button handler={this.exportToXlsx.bind(this)} text="Excel xlsx (all Items)"/>
+                    <Button handler={this.exportToXml.bind(this)} text="Excel xml (all Items)"/>
+                    <Button handler={this.exportToCSV.bind(this)} text="CSV (all Items)"/>
+                    <Button handler={this.exportToTSV.bind(this)} text="TSV (all Items)"/>
+                    <Button handler={this.exportToHtml.bind(this)} text="HTML (all Items)"/>
+                    <Button handler={() => this.refs.exportMenu.hide()} text="Cancel"/>
+                </ActionSheet>
+                <Grid
+                    ref="grid"
+                    title="Big Data Grid"
+                    store={this.store}
+                    shadow={true}
+                    plugins={[
+                        { type: 'grideditable' },
+                        { type: 'gridviewoptions' },
+                        { type: 'pagingtoolbar' },
+                        { type: 'summaryrow' },
+                        { type: 'columnresizing' },
+                        { type: 'rowexpander' },
+                        { type: 'multiselection' },
+                        { type: 'gridexporter' }
+                    ]}
+                    columns={[
+                        { 
+                            xtype: 'rownumberer' 
+                        }, {
+                            text: 'Id',
+                            dataIndex: 'employeeNo',
+                            flex: 1,
+                            minWidth: 100,
+                            exportStyle: {
+                                format: 'General Number',
+                                alignment: {
+                                    horizontal: 'Right'
+                                }
+                            }
+                        }, {
+                            text: 'Name',
+                            dataIndex: 'fullName',
+                            minWidth: 150
+                        }, {
+                            xtype: 'checkcolumn',
+                            headerCheckbox: true,
+                            dataIndex: 'verified',
+                            text: 'Verified'
+                        }, {
+                            xtype: 'gridheadergroup',
+                            text: 'Ratings',
+                            columns: [{
+                                text: 'Avg',
+                                xtype: 'numbercolumn',
+                                dataIndex: 'averageRating',
+                                width: 75,
+                                cell:{
+                                    cls:'big-data-ratings-cell'
+                                    //innerCls: '(get) => under4'
+                                }
+                                /*cell: {
+                                    cls: 'big-data-ratings-cell'}*/
+                                    //innerCls: '(get) => under5'
+                                    //innerCls: 'under5'
+                                    
+                            }, {
+                                text: 'All',
+                                dataIndex: 'rating',
+                                ignoreExport: true,
+                                cell: {
+                                    xtype: 'widgetcell',
+                                    forceWidth: true,
+                                    widget: {
+                                        xtype: 'sparklineline'
+                                    }
+                                }
+                            }]
+                        }, {
+                            text: 'Date of Birth',
+                            dataIndex: 'dob',
+                            editable: true,
+                            xtype: 'datecolumn',
+                            format: 'd-m-Y',
+                            // you can define an export style for a column
+                            // you can set alignment, format etc
+                            exportStyle: [{
+                                // no type key is defined here which means that me is the default style
+                                // that will be used by all exporters
+                                format: 'medium Date',
+                                alignment: {
+                                    horizontal: 'Right'
+                                }
+                            }, {
+                                // the type key means that me style will only be used by the csv exporter
+                                // and for all others the default one, defined above, will be used
+                                type: 'csv',
+                                format: 'Short Date'
+                            }]
+                        }, {
+                            text: '',
+                            width: 100,
+                            ignoreExport: true,
+                            cell: {
+                                xtype: 'widgetcell',
+                                widget: {
+                                    xtype: 'button',
+                                    ui: 'action',
+                                    //bind: 'Verify',// {record.firstName}', ExtJS bug in widgetcell
+                                    handler:onVerifyTap
+                                }
+                            }
+                        }, {
+                            text: 'Join Date',
+                            dataIndex: 'joinDate',
+                            editable: true,
+                            xtype: 'datecolumn',
+                            format: 'd-m-Y',
+                            exportStyle: {
+                                format: 'medium Date',
+                                alignment: {
+                                    horizontal: 'Right'
+                                }
+                            }
+                        },
+                        {
+                            text: 'Notice Period',
+                            dataIndex: 'noticePeriod',
+                            editable: true
+                        },
+                        {
+                            text: 'Email',
+                            dataIndex: 'email',
+                            editable: true,
+                            editor: {
+                                xtype: 'emailfield'
+                            },
+                            width: 250
+                        },
+                        {
+                            text: 'Absences',
+                            xtype: 'headergroup',
+                            columns: [{
+                                text: 'Illness',
+                                dataIndex: 'sickDays',
+                                align: 'center',
+                                summaryType: 'sum'
+                            }, {
+                                text: 'Holidays',
+                                dataIndex: 'holidayDays',
+                                align: 'center',
+                                summaryType: 'sum'
+                            }, {
+                                text: 'Holiday Allowance',
+                                dataIndex: 'holidayAllowance',
+                                align: 'center',
+                                summaryType: 'sum',
+                                summaryFormatter: 'number("0.00")',
+                                formatter: 'number("0.00")'
+                            }]
+                        },
+                        {
+                            text: 'Salary',
+                            dataIndex: 'salary',
+                            renderer: Ext.util.Format.usMoney,
+                            editable: true,
+                            width: 150,
+                            summaryType: 'sum',
+                            summaryRenderer: salarySummaryRenderer,
+                            exportStyle: {
+                                format: 'Currency',
+                                alignment: {
+                                    horizontal: 'Right'
+                                }
+                            }
+                        }
+                    ]}
+                    onBeforeDocumentSave={(view) => {
+                        view.mask({
+                            xtype: 'loadmask',
+                            message: 'Document is prepared for export. Please wait ...'
+                        })
+                    }}
+                    onDocumentSave={(view) =>
+                        view.unmask()
+                    }
+                >
+                    <Toolbar docked="top">
+                        <Button text="Export to..." handler={() => this.refs.exportMenu.show()}/>
+                    </Toolbar>
+                </Grid>
+            </Container>
         )
     }
 }
