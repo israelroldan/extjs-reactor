8b1f056ee9d8ff7868421409ae0d54002d1a3baf
Dan Buhrman <dan.buhrman@sencha.com>
2017-03-07 14:01:39 -0500

    3D column with renderer example.

--- packages/reactor-kitchensink/src/examples/index.js
+++ packages/reactor-kitchensink/src/examples/index.js
@@ -48,6 +48,7 @@ import BasicColumn from './BasicColumn/BasicColumn';
 import Basic3DColumn from './Basic3DColumn/Basic3DColumn';
 import Grouped from './Grouped/Grouped';
 import Stacked from './Stacked/Stacked';
+import ThreeDColumnWithRenderer from './3DColumnWithRenderer/3DColumnWithRenderer';
 import NegativeValues from './NegativeValues/NegativeValues';
 import Candlestick from './Candlestick/Candlestick';
 import OHLC from './OHLC/OHLC';
@@ -101,7 +102,8 @@ const root = {
                 { text: 'Basic 3D Column', component: Basic3DColumn },
                 { text: 'Grouped', component: Grouped },
                 { text: 'Stacked', component: Stacked },
-                { text: 'Negative Values', component: NegativeValues }
+                { text: 'Negative Values', component: NegativeValues },
+                { text: '3D Column With Renderer', component: ThreeDColumnWithRenderer }
             ] },
             { text: 'Financial', children: [
                 { text: 'Candlestick', component: Candlestick },
