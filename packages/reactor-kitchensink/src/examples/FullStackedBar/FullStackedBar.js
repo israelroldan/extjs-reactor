d9da11a069bf76fd26f71f85ca7787d2accfd9e6
AlexanderDracka <alexander.dracka@profiq.com>
2017-03-08 16:29:20 +0100

    Removed external Store from Full Stack Bar

--- packages/reactor-kitchensink/src/examples/FullStackedBar/FullStackedBar.js
+++ packages/reactor-kitchensink/src/examples/FullStackedBar/FullStackedBar.js
@@ -6,16 +6,6 @@ import createData from './createData';
 export default class FullStackedBar extends Component {
     constructor() {
         super();
-        //this.refresh();
-        /*this.onAxisLabelRender = (axis, label, layoutContext) => {
-        // Custom renderer overrides the native axis label renderer.
-        // Since we don't want to do anything fancy with the value
-        // ourselves except appending a '%' sign, but at the same time
-        // don't want to loose the formatting done by the native renderer,
-        // we let the native renderer process the value first.
-        console.log('checkpoint AxisLabelRenderer')
-        return layoutContext.renderer(label) + '%';
-    };*/
     }
 
     store = Ext.create('Ext.data.Store', {
