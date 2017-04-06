cb015b45e7141bfa794c3115399ddad706bad8e9
AlexanderDracka <alexander.dracka@profiq.com>
2017-04-06 14:32:35 +0200

    RowExpander: renamed

--- packages/reactor-kitchensink/src/examples/index.js
+++ packages/reactor-kitchensink/src/examples/index.js
@@ -8,7 +8,7 @@ import BigDataGrid from './Grid/AdvancedFeatures/BigData/Grid';
 import ReconfigureGrid from './Grid/AdvancedFeatures/ReconfigureGrid/Grid';
 import SummaryRowGrid from './Grid/AddonsDecorations/SummaryRow/Grid';
 import ViewOptionsGrid from './Grid/AddonsDecorations/ViewOptions/Grid';
-import RowExpanderGrid from './Grid/AddonsDecorations/RowExpander/Grid';
+import RowExpanderGrid from './Grid/AddonsDecorations/RowExpander/RowExpander';
 import ToolGrid from './Grid/AddonsDecorations/GridTools/GridTools';
 import ReduxGrid from './ReduxGrid/ReduxGrid';
 import PivotGrid from './PivotGrid/PivotGrid';
