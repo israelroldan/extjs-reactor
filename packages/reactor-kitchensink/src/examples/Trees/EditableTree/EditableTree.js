04915ef0c6a8973f97b62ce16ce51bbc32ba6ab8
AlexanderDracka <alexander.dracka@profiq.com>
2017-04-07 14:05:33 +0200

    Added EditableTree example

---
+++ packages/reactor-kitchensink/src/examples/Trees/EditableTree/EditableTree.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Tree, TreeColumn, TextColumn } from '@extjs/reactor/modern';
+import store from './Store.js';
+
+export default class EditableTreeExample extends Component{
+
+    render(){
+        return (
+            <Tree
+                title="Editable Tree"
+                height={400}
+                width={600}
+                plugins={[{type:'grideditable'}]}
+                store={store}
+            >
+                <TreeColumn 
+                    text="Name"
+                    dataIndex="text"
+                    flex={1}
+                    editable
+                />
+                <TextColumn
+                    text="Class Name"
+                    dataIndex="className"
+                    flex={1}
+                    editable
+                />
+            </Tree>
+        )
+    }
+}
