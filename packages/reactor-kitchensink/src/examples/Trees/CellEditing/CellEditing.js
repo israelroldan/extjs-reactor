0df55b0fa3414da541deacf7966a77c60b9cf263
AlexanderDracka <alexander.dracka@profiq.com>
2017-04-07 14:20:42 +0200

    Added CellEditing Tree example

---
+++ packages/reactor-kitchensink/src/examples/Trees/CellEditing/CellEditing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Tree, TreeColumn } from '@extjs/reactor/modern';
+import store from './Store';
+
+export default class CellEditingTreeExample extends Component {
+
+    render(){
+        return (
+            <Tree 
+                title="Cell Editing Tree"
+                height={400}
+                width={600}
+                store={store}
+                plugins={[
+                    {type:'gridcellediting'}
+                ]}
+            >
+                <TreeColumn
+                    text="Name"
+                    dataIndex="text"
+                    flex="1"
+                    editable
+                />
+            </Tree>
+        )
+    }
+}
