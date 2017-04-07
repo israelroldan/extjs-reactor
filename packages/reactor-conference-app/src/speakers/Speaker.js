4a36e3b927b1598fc25412df776f77d6e30b2b84
Dan Buhrman <dan.buhrman@sencha.com>
2017-04-07 09:04:37 -0400

    Add Speaker's events to speaker detail.

--- packages/reactor-conference-app/src/speakers/Speaker.js
+++ packages/reactor-conference-app/src/speakers/Speaker.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { Container } from '@extjs/reactor/modern';
+import { Container, Panel } from '@extjs/reactor/modern';
+import ScheduleList from '../schedule/ScheduleList';
 import { connect } from 'react-redux';
 import { setTitle } from '../actions';
 
 class Speaker extends Component {
 
     render() {
-        const { speaker } = this.props;
+        const { speakers, schedule } = this.props;
+        const speaker = speakers.speaker;
+        const scheduleStore = schedule.store;
         return (
-            <Container masked={!speaker} padding={20}>
+            <Container masked={!speaker} padding={20} layout="vbox">
                 { speaker && (
                     <div>
-                        <img className="app-speaker-image" src={speaker.image}/>
-                        <div className="app-speaker-text">
-                            <div className="app-speaker-name">{speaker.name}</div>
-                            <div className="app-speaker-title">{speaker.title}</div>
+                        <div className="app-speaker-ct">
+                            <img className="app-speaker-image" src={speaker.image}/>
+                            <div className="app-speaker-text">
+                                <div className="app-speaker-name">{speaker.name}</div>
+                                <div className="app-speaker-title">{speaker.title}</div>
+                            </div>
                         </div>
+                        <Panel title="Events" style={{paddingTop: '20px'}}>
+                            <ScheduleList
+                                dataStore={{
+                                    type: 'chained',
+                                    source: scheduleStore,
+                                    autoDestroy: true,
+                                    grouper: { property: 'time' },
+                                    filters: [{ property: 'speaker', value: speaker.name }]
+                                }}
+                            />
+                        </Panel>
                     </div>
                 )}
             </Container>
@@ -23,8 +39,8 @@ class Speaker extends Component {
     }
 }
 
-const mapStateToProps = ({speakers}) => {
-    return speakers;
+const mapStateToProps = ({speakers, schedule}) => {
+    return {speakers, schedule};
 }
 
 export default connect(mapStateToProps)(Speaker);
