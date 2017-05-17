0ccdf5a3bf386fa9bc4ab02be9436615ed0d3ec4
Mark Brocato <mark.brocato@sencha.com>
2017-05-17 16:39:42 -0400

    Undo some overnesting in the root layout on phones

--- packages/reactor-kitchensink/webpack.config.js
+++ packages/reactor-kitchensink/webpack.config.js
@@ -130,6 +130,8 @@ module.exports = function (env) {
         devServer: {
             contentBase: './build',
             historyApiFallback: true,
+            host: '0.0.0.0',
+            disableHostCheck: true,
             port: 8084,
             compress: isProd,
             inline: !isProd,
