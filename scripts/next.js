fc4e6ec1efbef9ca782b388a90e70f086a5afc67
Israel Roldan <israel.roldan@sencha.com>
2017-07-19 03:15:29 +0200

    Make sure ext-react* and sencha-cmd* packages are updated

--- scripts/next.js
+++ scripts/next.js
@@ -15,7 +15,7 @@ function updateToNext(dir) {
         if (!packageInfo.dependencies) return;
 
         const extReactPackages = Object.keys(packageInfo.dependencies)
-            .filter(name => name.match(/@extjs\/(ext-react|sencha-cmd)/))
+            .filter(name => name.match(/@extjs\/(ext-react|sencha-cmd).*/))
             .map(name => `${name}@next`)
 
         if (extReactPackages.length) {
