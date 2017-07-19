8b335fc8363fff7a468ad3f6a51c805c98f2edba
Israel Roldan <israel.roldan@sencha.com>
2017-07-19 04:06:40 +0200

    Update next.js

--- scripts/next.js
+++ scripts/next.js
@@ -14,14 +14,13 @@ function updateToNext(dir) {
 
         if (!packageInfo.dependencies) return;
 
-        const extReactPackages = Object.keys(packageInfo.dependencies)
-            .filter(name => name.match(/@extjs\/(ext-react|sencha-cmd).*/))
-            .map(name => `${name}@next`)
-
-        if (extReactPackages.length) {
-            const cmd = `npm i ${extReactPackages.join(' ')}`;
-            console.log(`[${dir}] ${cmd}`);
-            execSync(cmd, { cwd: dir });
-        }
+        Object.keys(packageInfo.dependencies)
+            .forEach((name) => {
+                if (name.match(/@extjs\/(ext-react|sencha-cmd).*/)) {
+                    packageInfo.dependencies[name] = 'next';
+                }
+            });
+        
+        fs.writeFileSync(packageFile, JSON.stringify(packageInfo, null, 2));
     }
 }
