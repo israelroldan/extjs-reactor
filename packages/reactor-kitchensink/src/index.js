8e6a809724c5701fe4717588b9d5cd19289180bd
Mark Brocato <mark.brocato@sencha.com>
2017-07-20 09:58:23 -0400

    Attempt to fix KS build

--- packages/reactor-kitchensink/src/index.js
+++ packages/reactor-kitchensink/src/index.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import React from 'react'
-import { AppContainer } from 'react-hot-loader'
 import { launch } from '@extjs/reactor';
 import App from './App'
 import Data from './Data';
